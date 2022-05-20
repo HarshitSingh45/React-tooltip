fdf9637d2d97127c4824f24ac3f703e9e16d94bc
HarshitSingh45 <harshitsing45@gmail.com>
2022-05-20 22:08:48 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,28 +4,37 @@ import Tooltip from "./Tooltip";
 export default function App() {
   return (
     <div className="App">
+      {/* heading */}
       <h1> React tooltip</h1>
-
+      {/* top hovering tooltip component which contains button and tooltip */}
       <div>
+        {/* passing content and direction as props */}
         <Tooltip content="Hovering at top!" direction="top">
+          {/* component child */}
           <button className="btn">Top Hover Button</button>
         </Tooltip>
       </div>
-
+      {/* bottom hovering tooltip component which contains button and tooltip */}
       <div>
+        {/* passing content and direction as props */}
         <Tooltip content="Hovering at bottom!" direction="bottom">
+          {/* component child */}
           <button className="btn">Bottom Hover Button</button>
         </Tooltip>
       </div>
-
+      {/* right hovering tooltip components which contains button and tooltip */}
       <div>
+        {/* passing content and direction as props */}
         <Tooltip content="Hovering at right!" direction="right">
+          {/* component child */}
           <button className="btn">Right Hover Button</button>
         </Tooltip>
       </div>
-
+      {/* left hovering tooltip components which contains button and tooltip */}
       <div>
+        {/* passing content and direction as props */}
         <Tooltip content="Hovering at left!" direction="left">
+          {/* component child */}
           <button className="btn">Left Hover Button</button>
         </Tooltip>
       </div>
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import "./Tooltip.css";
 
 const Tooltip = (props) => {
+  // hook to know whether mouse is hovering over the target
   const [active, setActive] = useState(false);
 
   return (
     <div
       className="TooltipWrapper"
+      // while hovering
       onMouseEnter={() => setActive(true)}
+      // when hovering stops
       onMouseLeave={() => setActive(false)}
     >
+      {/* displaying button */}
       {props.children}
+      {/* if hovering show tooltip */}
       {active && (
         <div className={`TooltipTip ${props.direction || "top"}`}>
           {props.content}
